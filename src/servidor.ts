bdfb0b086da55eb66b9f94315e8d6a1f7b99be18
Gith <alu0101463858@ull.edu.es>
2024-04-08 08:08:06 +0000

    cosas

--- src/servidor.ts
+++ src/servidor.ts
@@ -1,41 +1,11 @@
-/**
- * Servidor que recibe comandos y los ejecuta en el sistema operativo.
- *  - Escucha en el puerto 3000.
- * - Recibe comandos y los ejecuta en el sistema operativo.
- * - Devuelve la salida de la ejecución del comando al cliente.
- * - Si hay un error, devuelve el mensaje de error al cliente.
- * - Si hay un error en la ejecución del comando, devuelve el mensaje de error al cliente.
- */
-import net from 'net';
-import { exec } from 'child_process';
+import request from 'request';
 
-/**
- * Crea un servidor que escucha en el puerto 60300.
- * Cuando un cliente se conecta al servidor, recibe un comando del cliente.
- */
-net.createServer((socket) => {
-    console.log('Cliente conectado');
+const url = 'http://api.weatherstack.com/current?access_key=aeb97bf5fbae1e796215bb0be875d548&query=28.48,-16.31&units=m';
 
-    //socket.write('Conección establecida');
-    socket.on('data', (data) => {
-      const command = data.toString().trim();
-      console.log('Comando recibido:', command);
-      exec(command, (error, stdout, stderr) => { // Ejecuta el comando en el sistema operativo.
-        if (error) { // Si hay un error en la ejecución del comando, devuelve el mensaje de error al cliente.
-          socket.write(`Error: ${error.message}`);
-          return;
-        }
-        if (stderr) { // Si hay un error, devuelve el mensaje de error al cliente.
-          socket.write(`Error: ${stderr}`);
-          return;
-        }
-        socket.write(stdout); // Devuelve la salida de la ejecución del comando al cliente.
-      });
-    });
-
-    socket.on('close', () => {
-      console.log('Cliente desconectado');
-    });
-  }).listen(60300, () => {
-    console.log('Servidor escuchando en el puerto 60300');
-  });
+request({url: url, json: true}, (error: Error, response) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(response.body);
+  }
+});
